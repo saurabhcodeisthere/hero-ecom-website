2bfb0ff670ab9970097fc13e748408bfa31213fc
saurabhcodeisthere <48877546+saurabhcodeisthere@users.noreply.github.com>
2025-12-22 12:15:32 +0530

    Inventory Service Creation

---
+++ inventory-service/src/test/java/com/hero/bikestore/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hero.bikestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
